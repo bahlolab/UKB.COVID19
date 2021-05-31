509b9a451517c3bd63c7b381278325803fd8ac24
victoriaemilyjackson <jackson.v@wehi.edu.au>
2021-05-31 17:07:58 +1000

    generate agedCare variable
    generate agedCare variable

--- R/risk.factor.R
+++ R/risk.factor.R
@@ -3,22 +3,27 @@
 # ukb.data <- "/wehisan/bioinf/lab_bahlo/projects/misc/UKBiobank/data/app36610/rawPheno/ukb42082.tab"
 # ABO.data <- "/wehisan/bioinf/lab_bahlo/projects/misc/UKBiobank/COVID19/phenotypes/20200814_covid19_misc.txt"
 # inAgedCare.data <- "/wehisan/bioinf/lab_bahlo/projects/misc/UKBiobank/COVID19/AnalysisVEJ/APOE/apoe_careHome_pheno.txt"
-risk.factor <- function(ukb.data, ABO.data, inAgedCare.data, out.file = NULL){
-  db <- read.table(ukb.data, header = T, sep = "\t")
+risk.factor <- function(ukb.data, ABO.data, hesin.file, out.file = NULL){
+  # db <- read.table(ukb.data, header = T, sep = "\t")
+  # Use fread, to only read in relevant fields
+  db <- fread(ukb.data,
+    select=c("f.eid", "f.31.0.0", "f.21001.0.0", "f.21001.1.0", "f.21001.2.0", "f.21001.3.0", "f.21000.0.0", "f.21000.1.0", "f.21000.2.0", "f.22000.0.0", "f.189.0.0", "f.20161.0.0", "f.20161.1.0", "f.20161.2.0", "f.20161.3.0"), quote="") %>%
+    as.data.frame
+
   # sex: 1- male, 0- female
   phe <- db[,c("f.eid","f.31.0.0")]
   colnames(phe) <- c("ID","sex")
-  
+
   # age: 2020 - year of birth
   phe$age <- 2020-db$f.34.0.0
-  
+
   # Body mass index (BMI): the lastest
   bmi <- db[,startsWith(colnames(db),"f.21001.")]
   for(i in 2:ncol(bmi)){
     bmi[!(is.na(bmi[,i])),1] <- bmi[!(is.na(bmi[,i])),i]
   }
   phe$bmi <- bmi[,1]
-  
+
   # Ethnic background
   ethnic <- db[,startsWith(colnames(db),"f.21000.")]
   for(i in 2:ncol(ethnic)){
@@ -26,7 +31,7 @@ risk.factor <- function(ukb.data, ABO.data, inAgedCare.data, out.file = NULL){
   }
   if(any(ethnic[,1] <0 & !(is.na(ethnic[,1])))) ethnic[ethnic[,1] <0 & !(is.na(ethnic[,1])),1] <- NA
   phe$ethnic <- ethnic[,1]
-  
+
   phe$white <-  phe$mixed <- phe$asian <- phe$black <- phe$other.ppl <- 0
   phe[phe$ethnic %in% c(1001:1003,1) & !(is.na(phe$ethnic)),"white"] <- 1
   phe[phe$ethnic %in% c(2001:2004,2) & !(is.na(phe$ethnic)),"mixed"] <- 1
@@ -34,16 +39,16 @@ risk.factor <- function(ukb.data, ABO.data, inAgedCare.data, out.file = NULL){
   phe[phe$ethnic %in% c(4001:4003,4) & !(is.na(phe$ethnic)),"black"] <- 1
   phe[phe$ethnic == 6 & !(is.na(phe$ethnic)),"other.ppl"] <- 1
   phe[phe$ethnic %in% c(-1,-3),c("white","black","asian","mixed","other.ppl")] <- NA
-  
+
   ### batch effect
   array <-db[,'f.22000.0.0']
   phe$array <- NA
   phe[which(array<0 & !(is.na(array))),'array'] <-0
   phe[which(array>0 & !(is.na(array))),'array'] <-1
-  
+
   ### SES
   phe$SES <- db[,startsWith(colnames(db),"f.189.")]
-  
+
   ### smoking
   smoke <- db[,startsWith(colnames(db),"f.20161.")]
   for(i in 2:ncol(smoke)){
@@ -51,7 +56,7 @@ risk.factor <- function(ukb.data, ABO.data, inAgedCare.data, out.file = NULL){
   }
   smoke[is.na(smoke[,1]),1] <- 0
   phe$smoke <- smoke[,1]
-  
+
   ABO <- read.table(ABO.data,header = T, sep = "\t",stringsAsFactors = F)
   phe <- merge(phe,ABO,by.x = "ID", by.y = "eid",all = T)
   phe$A <- phe$B <- phe$AB <- phe$O <- 0
@@ -60,9 +65,22 @@ risk.factor <- function(ukb.data, ABO.data, inAgedCare.data, out.file = NULL){
   phe[phe$blood_group == "AB" & !(is.na(phe$blood_group)),"AB"] <- 1
   phe[phe$blood_group == "OO" & !(is.na(phe$blood_group)),"O"] <- 1
   phe[is.na(phe$blood_group),c("A","B","AB","O")] <- NA
-  
-  care.home <- read.table(inAgedCare.data,header = T)[,c(1,3)]
-  phe <- merge(phe,care.home,by.x = "ID", by.y = "ID",all = T)
+
+  # care.home <- read.table(inAgedCare.data,header = T)[,c(1,3)]
+  # phe <- merge(phe,care.home,by.x = "ID", by.y = "ID",all = T)
+
+
+  hesIn <- data.table::fread(hesin.file)
+
+  agedCare <- hesIn[, sourceAged := admisorc_uni %in% c(7000, 7001, 7002, 7003)] %>%
+    .[, dischargeAged := disdest_uni %in% c(7000, 7001, 7002, 7003)]
+
+  agedCareIds <- agedCare[sourceAged==T | dischargeAged==T , eid] %>% unique
+  noAgedCareIds <- agedCare[sourceAged==F & dischargeAged==F  & !(eid %in% agedCareIds), eid] %>% unique
+
+  phe[, "inAgedCare"] <- case_when(phe$ID %in% agedCareIds ~ 1,
+                    phe$ID %in% noAgedCareIds ~ 0,
+                    T ~ NA_real_)
   
   if(is.null(out.file)) out.file <- "covariate"
   write.table(phe,paste0(out.file,".txt"),row.names = F, quote = F, sep = "\t")
