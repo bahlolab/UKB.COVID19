c69988e489b653d2bae2ea1ed49c37e7ca9ec2ab
longfei-stat <54878372+longfei-stat@users.noreply.github.com>
2021-07-09 12:11:07 +1000

    Delete SAIGE_GWAS directory

--- SAIGE_GWAS/phe.pca.R
+++
@@ -1,9 +0,0 @@
-phe <- read.table("./data/phe.txt", header=T, sep="\t")
-pca <- read.table("./data/grmSNPsSubset/pca.20.eigenvec", header=F)[,-1]
-colnames(pca) <- c("ID",paste0("PC",1:20))
-phe.pca <- merge(phe,pca,by.x="ID",by.y="ID")
-ID.list <- cbind(phe.pca$ID,phe.pca$ID)
-write.table(phe.pca,"./data/phe.pca.txt",row.names = F, quote = F, sep = "\t")
-write.table(ID.list,"./data/ID.list.new",row.names = F, col.names = F, quote = F, sep = "\t")
-q()
-
